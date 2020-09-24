5c109bb1fb8aa3db85f8c55ff4457c04b5218608
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2020-09-24 10:07:52 -0300

    Aula 10 - Criando Tooltip de erros

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -21,13 +24,21 @@ export const Container = styled.div<ContainerProps>`
     margin-top: 8px;
   }
 
-  ${(props) => props.isFocused &&
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
     css`
       color: #ff9000;
       border-color: #ff9000;
     `}
 
-  ${(props) => props.isFilled &&
+  ${props =>
+    props.isFilled &&
     css`
       color: #ff9000;
     `}
@@ -57,5 +68,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #ffffff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
